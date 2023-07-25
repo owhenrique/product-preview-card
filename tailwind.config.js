83d5ded6f902a997fea1b105606b7d7aacc54a1e
owhenrique <me.pauloalmeida@gmail.com>
2023-07-25 02:16:11 -0300

    fix: button link

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -17,10 +14,8 @@ export default {
           grayish_blue: "hsl(228, 12%, 48%)",
           white: "hsl(0, 0%, 100%)",
         },
-
-      }
+      },
     },
   },
   plugins: [],
-}
-
+};
