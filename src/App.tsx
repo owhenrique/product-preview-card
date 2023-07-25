3544e12f2b2f11bb0a505a053480ca95ad65fbd4
owhenrique <me.pauloalmeida@gmail.com>
2023-07-25 02:04:08 -0300

    fix: deploy img path

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ function App() {
     <div className="bg-primary-cream flex min-h-screen p-5 justify-center items-center ">
       <div className="flex flex-1 flex-col lg:flex-row lg:max-h-[500px] lg:max-w-[600px] lg:rounded-xl overflow-hidden">
         <img
-          src="https://github.com/owhenrique/product-preview-card/blob/main/src/assets/img/mobile/product.jpg"
+          src="https://github.com/owhenrique/product-preview-card/blob/main/public/img/mobile/product.jpg"
           className="rounded-t-[0.75rem] lg:hidden mr-0"
           alt="Perfume bottle"
         />
         <img
-          src="https://github.com/owhenrique/product-preview-card/blob/main/src/assets/img/desktop/product.jpg"
+          src="https://github.com/owhenrique/product-preview-card/blob/main/public/img/desktop/product.jpg"
           className="rounded-t-[0.75rem] hidden lg:block lg:rounded-none w-[50%] mr-0"
           alt="Perfume bottle"
         />
